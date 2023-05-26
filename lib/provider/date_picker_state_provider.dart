0f523ea94dff0f27fb1e36a28af9951392962603
yuki2021 <ordinary.plusminus1@gmail.com>
2023-05-26 21:55:27 +0900

    [update]iosにfirebase_coreをインストール

--- lib/provider/date_picker_state_provider.dart
+++ lib/provider/date_picker_state_provider.dart
@@ -1,5 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:scrapbox_diary_app/scrapbox_utils/date_picker.dart';
+import 'package:scrapbox_diary_app/widget/date_picker.dart';
 
 final datePickerProvider =
     StateNotifierProvider<DatePickerState, DateTime>((ref) {
